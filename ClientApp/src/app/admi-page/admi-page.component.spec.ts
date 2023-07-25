1186e646b65625a02bbd501b4c0e55eb0accfb71
Sanchit Arora <Sanchit.Arora@cybergindia.local>
2023-07-25 23:24:10 +0530

    admin page changes

---
+++ ClientApp/src/app/admi-page/admi-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdmiPageComponent } from './admi-page.component';
+
+describe('AdmiPageComponent', () => {
+  let component: AdmiPageComponent;
+  let fixture: ComponentFixture<AdmiPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AdmiPageComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AdmiPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ClientApp/src/app/admi-page/admi-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admi-page',
+  templateUrl: './admi-page.component.html',
+  styleUrls: ['./admi-page.component.css']
+})
+export class AdmiPageComponent {
+
+}
