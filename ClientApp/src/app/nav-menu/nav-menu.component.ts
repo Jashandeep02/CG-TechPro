5701812fd0bccc285053e22805ec32d891f0e06a
harleenchadha12 <harleen.chadha@cginfinity.com>
2023-07-26 15:51:37 +0530

    nav-bar and user page created

--- ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,15 +1,26 @@
-// nav-menu.component.ts
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css'] // Add your custom CSS here if needed
+  styleUrls: ['./nav-menu.component.css']
 })
 export class NavMenuComponent {
-  isSlideOutVisible: boolean = false;
+  isSlideOutVisible = false;
+
+  @ViewChild('slideOutDiv', { static: false }) slideOutDiv!: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
 
   toggleSlideOut() {
     this.isSlideOutVisible = !this.isSlideOutVisible;
+
+    if (this.slideOutDiv) {
+      if (this.isSlideOutVisible) {
+        this.renderer.addClass(this.slideOutDiv.nativeElement, 'show-slide-out');
+      } else {
+        this.renderer.removeClass(this.slideOutDiv.nativeElement, 'show-slide-out');
+      }
+    }
   }
 }
